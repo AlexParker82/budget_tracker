9c7d3ea7426c542e959478f0662c28e9be044b92
Alex Parker <alex.n.parker82@gmail.com>
2021-11-04 20:58:21 -0400

    starting to work on service worker and caching

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,9 +17,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_MONGODB, {
   useNewUrlParser: true,
-  useFindAndModify: false
 });
 
 // routes
